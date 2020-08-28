bee3da57da44a17b2da45ca8033d5a95d667750c
Rafael Rodrigues <rafael.rodrigues.external@k2partnering.com>
2020-08-28 10:56:02 -0300

    commit initial

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfTeste
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HttpClient client = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            client.BaseAddress = new Uri("https://localhost:44357/api/");
+        }
+
+        private async void Salvar_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var pessoa = new Pessoa()
+                {
+                    Nome = txtNome.Text,
+                    Sobrenome = txtSobrenome.Text,
+                    Telefone = txtTelefone.Text
+                };
+                var response = await client.PostAsJsonAsync(@"https://web-wpfteste.azurewebsites.net/api/User", pessoa);
+                //var response = await client.PostAsJsonAsync(@"User", pessoa); 
+                response.EnsureSuccessStatusCode();
+                MessageBox.Show("Pessoa incluída com sucesso", "Result", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Erro ao criar o cadastro não foi incluído.");
+            }
+        } 
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        { 
+
+        }
+    }
+    
+}
---
+++ Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WpfTeste
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
